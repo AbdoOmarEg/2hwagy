2bf8eb00c2f576b9d7dbb31aa92eb7474be61a33
AbdoOrigin <abdelrahmanorigin@gmail.com>
2023-05-12 15:54:08 +0300

    added dark mode and header for today_matches, todo: add styling for the last two divs and add fonts from the main page

--- js/script.js
+++ js/script.js
@@ -1,29 +1,51 @@
 let loginForm = document.querySelector('.login-form');
 
-document.querySelector('#login-btn').onclick = () =>{
+document.querySelector('#login-btn').onclick = () => {
    loginForm.classList.add('active');
+   searchForm.classList.remove('active');
+   cartItem.classList.remove('active');
 }
 
-document.querySelector('#close-login-form').onclick = () =>{
+document.querySelector('#close-login-form').onclick = () => {
    loginForm.classList.remove('active');
 }
 
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.header .nav');
 
-menu.onclick = () =>{
+menu.onclick = () => {
    menu.classList.toggle('fa-times');
    navbar.classList.toggle('active');
+   searchForm.classList.remove('active');
+   cartItem.classList.remove('active');
 }
 
-window.onscroll = () =>{
+let searchForm = document.querySelector('.search-form')
+
+document.querySelector('#search-btn').onclick = () => {
+   searchForm.classList.toggle('active');
+   cartItem.classList.remove('active');
+   loginForm.classList.remove('active');
+   // navbar.classList.remove('active');
+   // as2l chatgpt ele fo2ya de bt3ml eh
+}
+
+let cartItem = document.querySelector('.cart-items-container')
+
+document.querySelector('#cart-btn').onclick = () => {
+   cartItem.classList.toggle('active');
+}
+
+window.onscroll = () => {
    loginForm.classList.remove('active');
    menu.classList.remove('fa-times');
    navbar.classList.remove('active');
+   searchForm.classList.remove('active');
+   cartItem.classList.remove('active');
 
-   if(window.scrollY > 0){
+   if (window.scrollY > 0) {
       document.querySelector('.header').classList.add('active');
-   }else{
+   } else {
       document.querySelector('.header').classList.remove('active');
    }
 }
